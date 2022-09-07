b4d01d719e62114c3fd33f887ec366edf17a6bcb
paschal533 <okwuosahpaschal@gmail.com>
2022-09-07 22:32:16 +0100

    first commit

--- Frontend/pages/fundraiser-details.js
+++ Frontend/pages/fundraiser-details.js
@@ -7,7 +7,7 @@ import { useTheme } from "next-themes";
 import { useRef } from "react";
 
 import { FundraiserContext } from "../context/FundraiserContext";
-import { shortenAddress } from "../utils/shortenAddress";
+//import { shortenAddress } from "../utils/shortenAddress";
 import { Button, Loader, Modal } from "../components";
 import images from "../assets";
 
