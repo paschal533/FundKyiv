8a685af04926b3e22b41115abb19b4b935142f52
andrey-ponamarev <ponamarevandrej@gmail.com>
2022-09-17 22:58:24 +0200

    refactor: Move fundraiser contract to the env file

--- Frontend/context/constants.js
+++ Frontend/context/constants.js
@@ -1,9 +1,3 @@
-import FundraiserFactory from "../contracts/FundraiserFactory.json";
-import Fundraiser from "../contracts/Fundraiser.json";
-
+// TODO: Move to the config file
 export const FundraiserFactoryAddress =
-  "0x255D2839bc54710de6aACB01c711420132d17616";
-
-export const FundraiserFactoryABI = FundraiserFactory.abi;
-
-export const FundraiserABI = Fundraiser.abi;
+  process.env.NEXT_PUBLIC_FUNDRAISER_CONTRACT_ADDRESS;
